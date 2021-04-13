f9e78a833b0728a19073af2d236c80e38dd177d5
github username <um_2003_8@hotmail.com>
2021-04-12 23:16:09 -0400

    create a helper function onMovieSelect and fetch data for individual movie to get all the details

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const onInput = async event => {
        option.addEventListener('click', ()=> {
            dropdown.classList.remove('is-active')
            input.value = movie.Title
+           onMovieSelect(movie)
        })
 
        resultsWrapper.appendChild(option)
@@ -62,3 +63,14 @@ document.addEventListener('click', event => {
         dropdown.classList.remove('is-active')
     }
 })
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd9835cc5',
+            i: movie.imdbID
+        }
+    })
+
+    console.log(response.data);
+}
