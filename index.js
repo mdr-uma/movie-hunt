e551aab7279494eb60a9cb9060b2f464772b34b7
github username <um_2003_8@hotmail.com>
2021-04-19 23:13:26 -0400

    call createAutoComplete funtion

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const fetchData = async searchTerm => {
     return response.data.Search
 }
 
+createAutoComplete({
+    root: document.querySelector('.autocomplete')
+})
+
 const onMovieSelect = async movie => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
