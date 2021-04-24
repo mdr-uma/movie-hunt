870cca493caf6f157bb9ae8797deafc08bd6f1fb
github username <um_2003_8@hotmail.com>
2021-04-23 22:27:26 -0400

    move onOptionSelect function to both autocomplet so that both can display movie

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const autoCompleteConfig = {
             ${movie.Title} (${movie.Year})
         `
     },
-    onOptionSelect(movie) {
-        document.querySelector('.tutorial').classList.add('is-hidden')
-        onMovieSelect(movie)
-    },
     inputValue(movie) {
         return movie.Title
     },
@@ -32,14 +28,22 @@ const autoCompleteConfig = {
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#left-autocomplete'),  
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#left-summary'))
+    }
 })
 
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#right-autocomplete'),  
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#right-summary'))
+    }
 })
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'd9835cc5',
@@ -47,7 +51,7 @@ const onMovieSelect = async movie => {
         }
     })
 
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data)
 }
 
 const movieTemplate = (movieDetail) => {
